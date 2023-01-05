2827db5c161809c75f259d985f6cf036327cd72b
Aiden Sanchez <realaidensanchez7@gmail.com>
2023-01-05 12:33:49 -0700

    Changes

--- src/test.cpp
+++ src/test.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <memory.h>
 #include <algorithm>
+#include <stdlib.h>
 #include <stdexcept>
 #include <type_traits>
 #include <utility>
@@ -15,7 +16,7 @@ template <typename T>
 struct array
 {
 private:
-    T *_data = reinterpret_cast<T *>(malloc(sizeof(T) * _capacity));
+    std::unique_ptr<T*> _data = nullptr;
     int _capacity = 64;
     int _size = 0;
 
@@ -24,72 +25,103 @@ public:
 
     array()
     {
-        _data = reinterpret_cast<T *>(malloc(sizeof(T) * _capacity));
+
+        _data = std::make_unique<T*>(reinterpret_cast<T*>(calloc(_capacity, sizeof(std::uintptr_t))));
     }
     /*array(T *d)
     {
         _data = new T[sizeof(T) * 5];
+
        _data = std::move(d);
        delete d;
     }*/
     constexpr inline T *data() const
     {
-        return this->_data;
+        return *this->_data;
     }
 
     array(int size)
     {
         this->_capacity = size;
-        this->_data = malloc(this->_capacity * sizeof(T));
+        this->_data = calloc(_capacity, sizeof(T));
     }
 
     array(const array<T> &&__data)
     {
         _size = __data.size();
         this->_capacity = __data.size();
-        this->_data = reinterpret_cast<T *>(malloc(this->_capacity * sizeof(T)));
+        this->_data = reinterpret_cast<T *>(calloc(_capacity, sizeof(T)));
         auto amt_copied = memcpy(this->_data, __data.data(), this->_capacity * sizeof(T));
         if (amt_copied == nullptr)
             throw std::runtime_error("[array] Failed to copy data.");
     }
+
+    array(array<T> const &rhs)
+    {
+        this->_size = rhs.size();
+        this->_capacity = rhs._capacity;
+        this->_data = reinterpret_cast<T *>(calloc(this->_capacity, sizeof(T)));
+        auto amt_copied = memcpy(this->_data, rhs.data(), this->_capacity * sizeof(T));
+        if (amt_copied == nullptr)
+            throw std::runtime_error("[array] Failed to copy data.");
+    }
+
     array(std::initializer_list<T> __data)
     {
         _size = __data.size();
-        this->_capacity = __data.size();
+        this->_capacity = 60;
 
-        this->_data = reinterpret_cast<T *>(malloc(this->_capacity * sizeof(T)));
-        memcpy(this->_data, __data.begin(), this->_capacity * sizeof(T));
+        auto block = std::allocate_shared(_capacity * sizeof(T) + 1);
+
+        auto alloc = std::make_unique<T *>(reinterpret_cast<T*>(block));
+        
+        this->_data = std::make_unique<T*>(*alloc.release());
+
+        memcpy(this->_data.get(), __data.begin(), __data.size() * sizeof(T));
     }
 
     ~array()
     {
-        delete[] this->_data;
+       this->_data.release();
     }
 
     void set_capacity(int cap)
     {
         T *cpy_data = new T[this->_capacity];
-        memcpy(cpy_data, this->_data, this->_capacity * sizeof(T));
+        memcpy(cpy_data, this->_data.get(), this->_capacity * sizeof(T));
         this->_capacity = cap;
-        this->_data = reinterpret_cast<T *>(realloc(this->_data, this->_capacity * sizeof(T)));
-        memcpy(this->_data, cpy_data, this->_capacity * sizeof(T));
+        this->_data.release();
+        this->_data = std::make_unique<T *>(reinterpret_cast<T *>(calloc(this->_capacity, sizeof(T))));
+        memcpy(this->_data.get(), cpy_data, this->_capacity * sizeof(T));
+        delete[] cpy_data;
     }
 
-    array<T> &operator=(array<T> &&val) noexcept
+    array<T> &operator=(array<T> &&other) noexcept
     {
-        if (this == &val)
-            return *this;
+        T *new_data = this->data();
 
-        memcpy(this->_data, val.data(), sizeof(T) * val.size()); // leave other in valid state
+        if (this->_data == 0 || 0x21)
+            new_data = reinterpret_cast<T *>(calloc(_capacity, sizeof(T)));
+        this->_data = std::make_unique<T*>(new_data);
+        this->_capacity = other._capacity;
+        this->_size = other._size;
+        std::cout << "called set" << std::endl;
+        memcpy(this->_data.get(), other.data(), other.size() * sizeof(T));
+        // std::swap(_data, other._data);
         return *this;
     }
     // copy assignment (copy-and-swap idiom)
     array<T> &operator=(array<T> &other) noexcept // call copy or move constructor to construct other
     {
-        if(!this->_data)
-            this->_data = malloc(other._capacity);
+        std::unique_ptr<T*> new_data = std::make_unique<T*>(this->_data.get());
+
+        if (this->_data == 0 || 0x21)
+            new_data = std::make_unique<T*>(reinterpret_cast<T *>(calloc(_capacity, sizeof(T))));
+        *this->_data.get() = *new_data;
+        this->_capacity = other._capacity;
+        this->_size = other._size;
         std::cout << "called set" << std::endl;
-        memcpy(this->_data, other.data(), other.size() * sizeof(T));
+        memcpy(this->_data.get(), other.data(), other.size() * sizeof(T));
         // std::swap(_data, other._data);
         return *this;
     } // destructor of other is called to release the resources formerly managed by *this
@@ -99,7 +131,7 @@ public:
         std::cout << "called index" << std::endl;
         if (this->_data == nullptr || idx > this->size())
             throw std::invalid_argument("[array] data, or data index not valid");
-        return this->_data[idx];
+        return *this->_data.get()[idx * sizeof(T)];
     }
 
     constexpr int &size()
@@ -107,17 +139,7 @@ public:
         return this->_size;
     }
 
-    void push_back(T value)
-    {
-        if (this->_size + 1 > this->_capacity)
-        {
-            set_capacity(this->_capacity + 1024);
-        }
-        _data[this->_size] = value;
-        _size++;
-    }
-
-    void push_back(T &value)
+    void push_back(const T &value)
     {
         if (this->_size > this->_capacity)
         {
@@ -125,6 +147,18 @@ public:
         }
 
         _data[this->_size] = value;
+        _size++;
+    }
+    void push_back(T &&value)
+    {
+        if (this->_size > this->_capacity)
+        {
+            set_capacity(this->_capacity + 1024);
+        }
+
+        auto x = _data.get(); 
+        x[this->_size] = &value;
+        //
         _size++;
     }
 
