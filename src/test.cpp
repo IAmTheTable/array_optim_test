f4562cb5ac6b505e7ced589a908454f877c06071
Table <table@table.com>
2023-01-06 14:33:47 -0700

    Changes

--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <memory.h>
+#include <string>
 #include <algorithm>
 #include <stdlib.h>
 #include <stdexcept>
 #include <type_traits>
 #include <utility>
-#include <bit>
+#include <bitset>
 // super fast and optimized array manipulation functions
 // inspired by the incredible std::vector, but way faster
 // and using a power of two instead of a generic size
@@ -18,14 +20,14 @@ private:
     std::allocator<T> _alloc;
     int _capacity = 64;
     int _size = 0;
-    T* _data = std::make_shared<T *>(reinterpret_cast<T *>(calloc(_capacity, sizeof(T))));
+    T *_data = _alloc.allocate(_capacity);
 
 public:
     /// @brief Default capacity of the array
 
     array()
     {
-        _data = std::make_shared<T *>(reinterpret_cast<T *>(calloc(_capacity, sizeof(std::uintptr_t))));
+        //_data = _alloc.allocate(_capacity);
     }
     /*array(T *d)
     {
@@ -36,7 +38,7 @@ public:
     }*/
     constexpr inline T *data() const
     {
-        return *this->_data;
+        return this->_data;
     }
 
     array(std::allocator_arg_t args)
@@ -46,37 +48,30 @@ public:
     array(int size)
     {
         this->_capacity = size;
-        this->_data = std::make_shared<T *>(reinterpret_cast<T *>(calloc(this->_capacity, sizeof(T))));
+       // _data = _alloc.allocate(_capacity);
     }
 
     array(const array<T> &&__data)
     {
-        _size = __data.size();
-        this->_capacity = __data.size();
-        this->_data = std::make_shared<T *>(reinterpret_cast<T *>(calloc(this->_capacity, sizeof(T))));
-        auto amt_copied = memcpy(*this->_data.get(), __data.data(), this->_capacity * sizeof(T));
-        if (amt_copied == nullptr)
-            throw std::runtime_error("[array] Failed to copy data.");
+        _size = __data._size;
+        this->_capacity = __data._capacity;
+        _data = _alloc.allocate(_capacity);
+        memcpy(this->_data, __data.data(), this->_capacity * sizeof(T));
     }
 
-    array(array<T> const &rhs)
+    array(array<T> &rhs)
     {
         this->_size = rhs._size;
         this->_capacity = rhs._capacity;
-        this->_data = std::make_shared<T *>(reinterpret_cast<T *>(calloc(this->_capacity, sizeof(T))));
-        auto amt_copied = memcpy(*this->_data.get(), rhs.data(), this->_capacity * sizeof(T));
-        if (amt_copied == nullptr)
-            throw std::runtime_error("[array] Failed to copy data.");
+        _data = _alloc.allocate(_capacity);
+        memcpy(this->_data, rhs.data(), this->_capacity * sizeof(T));
     }
 
     array(std::initializer_list<T> __data)
     {
-        _size = __data.size();
-        this->_capacity = 60;
-
-        this->_data = std::make_shared<T *>(reinterpret_cast<T *>(calloc(this->_capacity, sizeof(T))));
-
-        memcpy(*this->_data.get(), __data.begin(), __data.size() * sizeof(T));
+        this->_size = __data.size();
+        _data = _alloc.allocate(_capacity);
+        memcpy(this->_data, __data.begin(), this->_size * sizeof(T));
     }
 
     ~array()
@@ -85,36 +80,64 @@ public:
 
     void set_capacity(int cap)
     {
+        auto old_capacity = this->_capacity;
         T *cpy_data = new T[this->_capacity];
-        memcpy(cpy_data, this->_data.get(), this->_capacity * sizeof(T));
+        // memcpy(cpy_data, this->_data, this->_capacity * sizeof(T));
+        _alloc.deallocate(_data, 1);
         this->_capacity = cap;
-        this->_data.reset();
-        this->_data = std::make_shared<T *>(reinterpret_cast<T *>(reallocarray(this->_data.get(), this->_capacity, sizeof(T))));
-        memcpy(this->_data.get(), cpy_data, this->_capacity * sizeof(T));
+        _data = _alloc.allocate(_capacity);
+        this->_data = std::ref(cpy_data);
         delete[] cpy_data;
     }
+    /*
+        array<T> &operator=(const array<T> &other) noexcept
+        {
+            // Guard self assignment
+            if (this == &other)
+                return *this;
 
-    array<T> &operator=(array<T> &&other) noexcept
+            // assume *this manages a reusable resource, such as a heap-allocated buffer mArray
+            if (this->_capacity != other._capacity) // resource in *this cannot be reused
+            {
+                _alloc.deallocate(this->_data, this->_capacity); // release resource in *this
+                _data = nullptr;
+                // size = 0;                     // preserve invariants in case next line throws
+                _data = _alloc.allocate(other._capacity); // allocate resource in *this
+                _capacity = other._capacity;
+                _size = other._size;
+            }
+
+            std::copy(other._data, other._data + sizeof(other), this->_data);
+            return *this;
+        }
+
+        array<T> &operator=(array<T> &&other) noexcept
+        {
+            // Guard self assignment
+            if (this == &other)
+                return *this; // delete[]/size=0 would also be ok
+            delete[] _data;
+            this->_data = std::exchange(other._data, nullptr); // leave other in valid state
+            this->_capacity = std::exchange(other._capacity, 0);
+            this->_size = std::exchange(other._size, 0);
+            return *this;
+        }*/
+
+    // copy assignment (copy-and-swap idiom)
+    array<T> &operator=(array<T> other) noexcept // call copy or move constructor to construct other
     {
-        T *new_data = this->data();
-
-        if (this->_data == 0 || 0x21)
-            new_data = reinterpret_cast<T *>(calloc(_capacity, sizeof(T)));
-        this->_data = std::make_shared<T *>(new_data);
-        this->_capacity = other._capacity;
-        this->_size = other._size;
-        std::cout << "called set" << std::endl;
-        memcpy(this->_data.get(), other.data(), other.size() * sizeof(T));
-        // std::swap(_data, other._data);
+        std::swap(this->_size, other._size); // exchange resources between *this and other
+        std::swap(this->_data, other._data);
+        std::swap(this->_capacity, other._capacity);
         return *this;
-    }
+    } // destructor of other is called to release the resources formerly managed by *this
 
     T &operator[](int idx)
     {
         std::cout << "called index" << std::endl;
         if (this->_data == nullptr || idx > this->size())
             throw std::invalid_argument("[array] data, or data index not valid");
-        return *this->_data.get()[idx * sizeof(T)];
+        return this->_data[idx * sizeof(T)];
     }
 
     constexpr int &size()
@@ -134,12 +157,14 @@ public:
     }
     void push_back(T &&value)
     {
-        auto val = std::make_shared<T>(value);
-        if (this->_size > this->_capacity)
+        if (this->_size + 1 > this->_capacity)
         {
             set_capacity(this->_capacity + 1024);
         }
-        memcpy(_data.get()[this->_size], val.get(), sizeof(T));
+        //*(this->_data + (this->_size * sizeof(T))) = value;
+        // this->_data[this->_size] = value;
+        this->_data[this->_size] = value;
+
         //
         _size++;
     }
@@ -149,7 +174,7 @@ public:
         if (this->_size + value.size() > this->_capacity)
         {
             set_capacity(this->_capacity + value.size() + 1024); // allocate the expected size plus 5 more
-            memcpy(this->_data + (sizeof(T) * this->_size), value.data(), value.size() * sizeof(T));
+            memcpy(this->_data[this->_size], value.data(), value.size() * sizeof(T));
         }
         else
             throw std::runtime_error("[array]Push range fail.");
